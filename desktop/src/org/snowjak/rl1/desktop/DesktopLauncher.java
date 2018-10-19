8d695cc77b8b6e699427f4c35b3cd3364b879cb4
Steve Nowak <rr247200@fincoad.com>
2018-10-19 10:42:26 -0400

    Initial commit.

---
+++ desktop/src/org/snowjak/rl1/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package org.snowjak.rl1.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import org.snowjak.rl1._App;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new _App(), config);
+	}
+}
